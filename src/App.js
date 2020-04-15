1934789571a63d93a27bcecea2f306c64b6be1a8
Benoit Tigeot <benoit@hopsandfork.com>
2020-04-15 15:52:31 +0200

    Add basic flexbox

--- src/App.js
+++ src/App.js
@@ -14,18 +14,39 @@ class App extends Component {
     Tabletop.init({
       key: 'https://docs.google.com/spreadsheets/d/111_7rU2qfo73sna0_ZcFt9QLFqTPl9LqevS3FIfHlio/edit#gid=0',
       callback: googleData => {
-        console.log('google sheet data --->', googleData)
+        this.setState({
+          data: googleData
+        })
       },
       simpleSheet: true
     })
   }
 
   render() {
+    const { data } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Testing</h1>
+          <h1 className="event-title">Nantes Beer Festival</h1>
+          <div className="event-date">
+            <h2>1 - 2 septembre 2020</h2>
+          </div>
         </header>
+        <div id="event-list">
+          <ul>
+            {
+              data.map(obj => {
+                return (
+                  <li key={obj.Nom}>
+                    <p>Le {obj.Jours} à {obj.Horaires} avec {obj.Brasserie}</p>
+                    <p>Lieu: {obj.Lieu}</p>
+                    <p>Horaires: {obj.Horaires}</p>
+                  </li>
+                )
+              })
+            }
+         </ul>
+        </div>
       </div>
     );
   }
